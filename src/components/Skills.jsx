18321bdc2f716b211c55866cc50e869daeac8ae4
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2023-10-03 00:39:13 +0330

    skills added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,62 @@ import React from 'react'
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <div className='mb-8'>
+      <h1 className='uppercase font-bold text-xl md:text-3xl mb-8 text-center'>My Skills</h1>
+      <div className='flex flex-wrap gap-4 items-center justify-center'>
+
+        <div className='w-52 h-52 border border-purple-700 p-5 flex flex-col items-center justify-center rounded-lg bg-purple-100 hover:bg-purple-50 hover:shadow-2xl transition-all duration-500 ease-out'>
+          <div className='h-32'>
+            <img className='w-20 h-20' src="../src/assets/htmlcss.png" alt="" />
+          </div>
+          <div className='h-20'>
+            <h2 className='text-sm text-center font-bold mb-5'>HTML/CSS</h2>
+            <h3 className='text-sm text-center font-bold'>Advanced</h3>
+          </div>
+        </div>
+
+        <div className='w-52 h-52 border border-purple-700 p-5 flex flex-col items-center justify-center rounded-lg bg-purple-100 hover:bg-purple-50 hover:shadow-2xl  transition-all duration-500 ease-out'>
+          <div className='h-32'>
+            <img className='w-20 h-20' src="../src/assets/tailwindicon.png" alt="" />
+          </div>
+          <div className="h-20">
+            <h2 className='text-sm text-center font-bold mb-5'>Tailwindcss</h2>
+            <h3 className='text-sm text-center font-bold'>Advanced</h3>
+          </div>
+        </div>
+
+        <div className='w-52 h-52 border border-purple-700 p-5 flex flex-col items-center justify-center rounded-lg bg-purple-100 hover:bg-purple-50 hover:shadow-2xl  transition-all duration-500 ease-out'>
+          <div className='h-32'>
+            <img className='w-20 h-20' src="../src/assets/jsicon.png" alt="" />
+          </div>
+          <div className="h-20">
+            <h2 className='text-sm text-center font-bold mb-5'>Javascript</h2>
+            <h3 className='text-sm text-center font-bold'>Competent</h3>
+          </div>
+        </div>
+
+        <div className='w-52 h-52 border border-purple-700 p-5 flex flex-col items-center justify-center rounded-lg bg-purple-100 hover:bg-purple-50 hover:shadow-2xl  transition-all duration-500 ease-out'>
+          <div className='h-32'>
+            <img className='w-20 h-20' src="../src/assets/npmicon.png" alt="" />
+          </div>
+          <div className="h-20">
+            <h2 className='text-sm font-bold text-center mb-5'>Working with npm pachages</h2>
+            <h3 className='text-sm text-center font-bold'>Advanced</h3>
+          </div>
+        </div>
+
+        <div className='w-52 h-52 border border-purple-700 p-5 flex flex-col items-center justify-center rounded-lg bg-purple-100 hover:bg-purple-50 hover:shadow-2xl  transition-all duration-500 ease-out'>
+          <div className='h-32'>
+            <img className='w-20 h-20' src="../src/assets/reacticon.png" alt="" />
+          </div>
+          <div className="h-20">
+            <h2 className='text-sm text-center font-bold mb-5'>React.js</h2>
+            <h3 className='text-sm text-center font-bold'>Beginner</h3>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
